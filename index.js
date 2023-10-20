15825d74d29fd63cfb9711105658d0991e40f77d
Shubham Tandon <iamshubham316@gmail.com>
2023-10-20 14:03:49 +0530

    adding live url

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.listen(PORT, () => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://cheery-blini-93a516.netlify.app/"],
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     credentials: true,
     exposedHeaders: ['Set-Cookie'],
